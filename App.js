fd3506a1e4d56eee841880f6c3235ffb8a1936e6
dnizfor <dnizfor@gmail.com>
2025-01-18 05:43:48 +0300

    remove player

--- App.js
+++ App.js
@@ -2,21 +2,53 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import TabStack from './src/stacks/TabStack';
-import MusicPlayerScreen from './src/screens/musicScreen/MusicPlayerScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import ChatScreen from './src/screens/ChatScreen';
+import SettingScreen from './src/screens/SettingScreen';
+import colors from './src/utilities/colors';
+import FeedScreen from './src/screens/FeedScreen';
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
+
+  const screenOptions = ({ route }) => ({
+    tabBarIcon: ({ focused, color, size }) => {
+      let iconName;
+
+      if (route.name === "Feed") {
+        iconName = focused ? "bookmark" : "bookmark-outline";
+        return (
+          <Ionicons name={iconName} size={size} color={color} />
+        );
+      } else if (route.name === "Chat") {
+        iconName = focused ? "bonfire" : "bonfire-outline";
+        return <Ionicons name={iconName} size={size} color={color} />;
+      } else if (route.name === "Settings") {
+        iconName = focused ? "settings-sharp" : "settings-outline";
+        return <Ionicons name={iconName} size={size} color={color} />;
+      }
+
+    },
+    tabBarActiveTintColor: colors.primary,
+    tabBarInactiveTintColor: colors.white,
+    tabBarHideOnKeyboard: false,
+    headerShown: false,
+    tabBarStyle: { backgroundColor: colors.black },
+    tabBarLabel: '',
+
+  });
+
+  const Tab = createBottomTabNavigator();
 
   return (
     <SafeAreaProvider>
 
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }} >
-          <Stack.Screen name="MusicHome" component={TabStack} />
-          <Stack.Screen name="MusicPlayer" component={MusicPlayerScreen} />
-        </Stack.Navigator>
+        <Tab.Navigator screenOptions={screenOptions}>
+          <Tab.Screen name="Feed" component={FeedScreen} />
+          <Tab.Screen name="Chat" component={ChatScreen} />
+          <Tab.Screen name="Settings" component={SettingScreen} />
+        </Tab.Navigator>
         <StatusBar hidden style='dark' />
       </NavigationContainer >
 
