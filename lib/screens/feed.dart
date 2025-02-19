26083d11acd8f13ea78a7b9e2ccced0e8598dd58
Deniz <dnizfor@gmail.com>
2025-02-20 00:42:40 +0300

    add loading to feed screen

--- lib/screens/feed.dart
+++ lib/screens/feed.dart
@@ -5,6 +5,7 @@ import 'package:zihnai/ultils/constant/color.dart';
 import 'package:zihnai/ultils/providers/feed_provider.dart';
 import 'package:zihnai/ultils/services/api_services.dart';
 import 'package:zihnai/widgets/feed_card.dart';
+import 'package:zihnai/widgets/loading_widget.dart';
 
 class FeedScreen extends StatefulWidget {
   const FeedScreen({super.key});
@@ -22,15 +23,18 @@ class _FeedScreenState extends State<FeedScreen> {
     final feedList = context.watch<FeedProvider>().feedList;
     return Container(
       color: HexColor(dark),
-      child: PageView.builder(
-          scrollDirection: Axis.vertical,
-          onPageChanged: (index) => {
-                if (index > feedList.length - 5)
-                  {context.read<FeedProvider>().updateFeedList()}
-              },
-          itemCount: feedList.length,
-          controller: pageController,
-          itemBuilder: (context, index) => (FeedCard(title: feedList[index]))),
+      child: feedList.isEmpty
+          ? LoadingWidget()
+          : PageView.builder(
+              scrollDirection: Axis.vertical,
+              onPageChanged: (index) => {
+                    if (index > feedList.length - 5)
+                      {context.read<FeedProvider>().updateFeedList()}
+                  },
+              itemCount: feedList.length,
+              controller: pageController,
+              itemBuilder: (context, index) =>
+                  (FeedCard(title: feedList[index]))),
     );
   }
 }
