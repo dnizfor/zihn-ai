90d46ab9cb579d95a944d11cab56821f0752746f
Deniz <dnizfor@gmail.com>
2025-02-19 15:57:12 +0300

    update mail

--- lib/ultils/functions/send_mail.dart
+++ lib/ultils/functions/send_mail.dart
@@ -3,7 +3,7 @@ import 'package:flutter_email_sender/flutter_email_sender.dart';
 Future<void> sendMail(String subject) async {
   final Email email = Email(
     subject: subject,
-    recipients: ['team@lingovido.com'],
+    recipients: ['zihnai@lingovido.com'],
     isHTML: false,
   );
 
